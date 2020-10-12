3d2dafcf582add02a70a4b332d8f84a2546636e1
juliyaAA <yulya919@yandex.ru>
2020-10-12 22:01:50 +0300

    23-1.Ajax

--- js/ajax.js
+++ js/ajax.js
@@ -13,17 +13,17 @@ function getXmlRequest (url, callback) {
         if(xhr.status !== 200) {
             console.log('error');
         }
-    }
+    };
     xhr.send();
 }
 
-function sleep() {
-    for (i = 0; i < 100000000; i++) {
+// function sleep() {
+//     for (i = 0; i < 100000000; i++) {
 
-    }
-}
+//     }
+// }
 
-jQuery(($) => {
+jQuery(document).ready(($) => {
     $('#city_name').on('click', function(e) {
         e.preventDefault();
         $.fancybox.open({
@@ -38,25 +38,25 @@ jQuery(($) => {
             });
         }
     });
-});
-$('[name = city_choose]').on('keyup', function() {
-    let search = $(this).val(),
-    result = '<ul>',
-    counter = 0;
-    for(let i = 0; i < cities.length; i++) {
-        if(cities[i].name.toLowerCase().indexOf(search.toLowerCase()) >= 0){
-            result += '<li>' + cities[i].name + '</li>'; 
-        }
-    }
-    result += '</ul>';
-    if(!counter) {
-        result = 'Ничего не найдено';
-    }
-    $('#search_result').html(result);
-    $('body').on('click', '#search_result li', function (){
-        $('#city_name').html($(this).html());
-        $fancybox.close();
-    })
+    $('[name = city_choose]').on('keyup', function() {
+            let search = $(this).val(),
+            result = '<ul>',
+            counter = 0;
+            for(let i = 0; i < cities.length; i++) {
+                if(cities[i].name.toLowerCase().indexOf(search.toLowerCase()) >= 0){
+                    result += '<li>' + cities[i].name + '</li>'; 
+                }
+            }
+            result += '</ul>';
+            if(!counter) {
+                result = 'Ничего не найдено';
+            }
+            $('#search_result').html(result);
+            $('body').on('click', '#search_result li', function (){
+                $('#city_name').html($(this).html());
+                $fancybox.close();
+            });
+        });
 });
 jQuery(document).ready(($) => {
     $('.ajax-loader').show();
@@ -65,7 +65,9 @@ jQuery(document).ready(($) => {
         type: 'GET',
         dataType: 'json',
         success: function(result){
-            sleep();
+            setTimeout(() => {
+                $('.ajax-loader').hide();
+            }, 3000);
             console.log(result);
             city = result.city.name_ru;
             console.log(city);
@@ -77,3 +79,23 @@ jQuery(document).ready(($) => {
         }
     });
 });
+// jQuery(document).ready(($) => {
+//     $('.ajax-loader').show();
+//     $.ajax ({
+//         url: GEO_API,
+//         type: 'GET',
+//         dataType: 'json',
+//         success: function(result){
+//             console.log(result);
+//             city = result.city.name_ru;
+//             console.log(city);
+//             $('#city_name').html(city);
+//             $('.ajax-loader').hide();
+//         },
+//         error: function(result) {
+//             setTimeout(() => {
+//                 $('.ajax-loader').hide();
+//             }, 3000);
+//         }
+//     });
+// });
